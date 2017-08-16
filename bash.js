7fa6d918157e7b5c92dda3fc6ea5b7a730aad62e
Fahad Billah <fahadbillah@yahoo.com>
2017-08-17 04:30:36 +0600

    some update

--- bash.js
+++ bash.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
 const chalk = require('chalk');
 
 var argv = [];
+const templatePath = './template';
 
-(function(){
+var _readArgs = function(){
   process.argv.slice(2).forEach(function (argument, index) {
     console.log(argument)
     // console.log(index)
@@ -20,15 +24,29 @@ var argv = [];
       argv.push({
         option: a,
         value: process.argv.slice(2)[index+1]
-      })
+      });
 
       // console.log(chalk.red.bgYellow.bold(process.argv.slice(2)[index+1]));
     // } else {
     //   console.log(chalk.blue(argument));
     }
   });
-})();
+};
 
-console.log(argv);
+var _createFolder = function() {
+  mkdirp('/output/' + argv[0].value, function (err) {
+    if (err) console.error(chalk.red(err))
+    else console.log(chalk.green('pow!'))
+  });
+};
+
+var _init = function() {
+  _readArgs();
+  _createFolder();
+};
+
+_init();
+
+console.log(argv[0]);
 
 // console.log(chalk.blue('Hello world!'));
